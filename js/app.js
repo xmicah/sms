46d2987fa3d20ead7a1ca13bb1039ea820c4418b
Micah Johnson <xmicah@gmail.com>
2013-09-10 13:20:09 -0400

    Color scale change
    Use actual data to calculate color scale.

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ var app = angular.module('xoApp', []);
 
 app.controller('cupController', function cupController($scope, $http) {
 
+    // Initialization
     $scope.show_quartz = true;
     $scope.show_cal = true;
     $scope.show_mtbstfa = true;
@@ -11,8 +12,13 @@ app.controller('cupController', function cupController($scope, $http) {
     $scope.colorby = "kind";
     $scope.colorbrew = "Reds";
 
+    $scope.colorScale = d3.scale.ordinal()
+        .domain(['q','c','m','t'])
+        .range(["orange","blue","purple","green"]);
+
+    $scope.colorbrews = ["Reds", "Greens", "Blues", "Purples", "Oranges", "BuGn"];
+
     var isCupVisible = null;
-    var colorByCupNum = null;
 
     // Setup cup initial cup data
     $scope.cup = [];
@@ -23,92 +29,6 @@ app.controller('cupController', function cupController($scope, $http) {
         $scope.cup[i].kind = '';
         $scope.cup[i].maxload = 0;
         $scope.cup[i].numloads = 0;
-    }
-
-    var getCupColor = function (cupnum) {
-        if ( isCupVisible(cupnum) ) {
-            return colorByCupNum(cupnum);
-        }
-        return "gray";
-    }
-
-    // Based on cup kind
-    // Return true if visible, false if not
-    var visibleByKind = function(cupnum)
-    {
-        var kind = $scope.cup[cupnum-1].kind;
-        var visible = ( kind == "q" && $scope.show_quartz )
-            || ( kind == "c" && $scope.show_cal )
-            || ( kind == "m" && $scope.show_mtbstfa )
-            || ( kind == "t" && $scope.show_tmah );
-        return visible;
-    }
-
-    // Return specific color for cup kind
-    var colorByKind = function(cupnum)
-    {
-        var kind = $scope.cup[cupnum-1].kind;
-        var color = "black";
-        switch (kind) {
-            case 'q':
-                color = "orange";
-                break;
-            case 'm':
-                color = "purple";
-                break;
-            case 't':
-                color = "green";
-                break;
-            case 'c':
-                color = "blue";
-                break;
-            default:
-                color = "yellow";
-        }
-        return color;
-    }
-
-    $scope.colorScale = d3.scale.linear().domain([100,250]).range(["#ffcccc", "#660000"]);
-    // $scope.redLoadScale = d3.scale.linear().domain([100,250]).range(["#ffcccc", "#660000"]);
-    // $scope.purpleLoadScale = d3.scale.quantize()
-    //     .domain([100, 250])
-    //     .range(colorbrewer.YlGnBu[9]);
-
-    var colorByLoad = function(cupnum)
-    {
-        return $scope.colorScale($scope.cup[cupnum-1].maxload);
-    }
-
-    var updateColorBy = function()
-    {
-        if ( $scope.colorby == "kind" ) {
-            colorByCupNum = colorByKind;
-        } else if ( $scope.colorby == "maxload" ) {
-            colorByCupNum = colorByLoad;
-        }
-        $scope.refresh();
-    }
-
-    var updateColorbrew = function()
-    {
-        $scope.colorScale.range(colorbrewer[$scope.colorbrew][9]);
-        $scope.refresh();
-    }
-
-    $scope.refresh = function()
-    {
-        groupCupLabels.selectAll(".cuplbl")
-            .style("visibility", function (d) {
-                return isCupVisible(d.cup) ? "visible" : "hidden";
-            })
-            .style("fill", function (d) {
-                var hsl = d3.hsl(getCupColor(d.cup));
-                return hsl.l > 0.6 ? "black" : "white";
-            });
-        groupCups.selectAll(".cup")
-            .attr("fill", function (d) {
-                return getCupColor(d.cup);
-            });
     }
 
     // Read static cup type file and update cup data
@@ -133,17 +53,91 @@ app.controller('cupController', function cupController($scope, $http) {
         $scope.logLoaded = true;
     });
 
+    // Private methods
+    var getCupColor = function (cupnum) {
+        if ( isCupVisible(cupnum) ) {
+            return $scope.colorScale($scope.cup[cupnum-1][$scope.colorby]);
+        }
+        return "gray";
+    }
+
+    // Based on cup kind
+    // Return true if visible, false if not
+    var visibleByKind = function(cupnum)
+    {
+        var kind = $scope.cup[cupnum-1].kind;
+        var visible = ( kind == "q" && $scope.show_quartz )
+            || ( kind == "c" && $scope.show_cal )
+            || ( kind == "m" && $scope.show_mtbstfa )
+            || ( kind == "t" && $scope.show_tmah );
+        return visible;
+    }
+
+    var updateColorScale = function()
+    {
+        var stats = {
+            hist: {}
+        };
+        var useOrdinal = false;
+
+        for ( var cupnum = 0, entry; entry = $scope.cup[cupnum++]; ) {
+
+            var value = $scope.cup[cupnum-1][$scope.colorby];
+            if ( isNaN(value) ) useOrdinal = true; // If non-numbers, use ordinal
+            if ( !("min" in stats) || value < stats.min ) stats.min = value;
+            if ( !("max" in stats) || value > stats.max ) stats.max = value;
+            if ( value in stats.hist ) {
+                stats.hist[value] ++;
+            } else {
+                stats.hist[value] = 1;
+            }
+        }
+        var values = [];
+        for (var key in stats.hist ) {
+            if ( stats.hist.hasOwnProperty(key) ) values[values.length] = key;
+        }
+        var ncolors = values.length;
+        if ( ncolors < 2 ) return;
+        if ( ncolors > 9 ) ncolors = 9;
+        if ( useOrdinal ) {
+            $scope.colorScale = d3.scale.ordinal()
+                .domain(values)
+                .range(colorbrewer[$scope.colorbrew][ncolors]);
+        } else if ( values.length > 9 ) {
+            $scope.colorScale = d3.scale.quantize()
+                .domain(values)
+                .range(colorbrewer[$scope.colorbrew][ncolors]);
+        } else {
+            $scope.colorScale = d3.scale.quantile()
+                .domain(values)
+                .range(colorbrewer[$scope.colorbrew][ncolors]);
+        }
+        $scope.refresh();
+    }
+
+    $scope.refresh = function()
+    {
+        groupCupLabels.selectAll(".cuplbl")
+            .style("visibility", function (d) {
+                return isCupVisible(d.cup) ? "visible" : "hidden";
+            })
+            .style("fill", function (d) {
+                var hsl = d3.hsl(getCupColor(d.cup));
+                return hsl.l > 0.6 ? "black" : "white";
+            });
+        groupCups.selectAll(".cup")
+            .attr("fill", function (d) {
+                return getCupColor(d.cup);
+            });
+    }
     // Trigger refresh on state change
     $scope.$watch('kindLoaded', $scope.refresh);
     $scope.$watch('logLoaded', $scope.refresh);
-    $scope.$watch('colorby', updateColorBy);
-    $scope.$watch('colorbrew', updateColorbrew);
+    $scope.$watch('colorby', updateColorScale);
+    $scope.$watch('colorbrew', updateColorScale);
 
     // Initial functor settings
     isCupVisible = visibleByKind;
-    //colorByCupNum = colorByLoad;
-    colorByCupNum = colorByKind;
-    $scope.colorScale = d3.scale.quantize().domain([100,250]).range(colorbrewer[$scope.colorbrew][6]);
 
     // $scope.vizcups = function()
     // {
